56901d17d001c7fa0a7ad0a45f61130795f235c2
haruki7049 <tontonkirikiri@gmail.com>
2026-01-20 12:50:02 +0900

    Update sine.zig

--- src/basic/sine.zig
+++ src/basic/sine.zig
@@ -2,42 +2,48 @@ const std = @import("std");
 const lightmix = @import("lightmix");
 const Wave = lightmix.Wave;
 
-pub const GenerateOptions = struct {
+pub const GenOptions = struct {
     frequency: f32,
     amplitude: f32,
     length: usize,
     allocator: std.mem.Allocator,
 
-    sample_rate: usize,
-    channels: usize,
-    bits: usize,
+    sample_rate: u32,
+    channels: u16,
 };
 
-pub fn generate(options: GenerateOptions) Wave {
-    const sample_rate: f32 = @floatFromInt(options.sample_rate);
-    const data: []const f32 = generate_data(options.frequency, options.amplitude, options.length, sample_rate, options.allocator);
-    defer options.allocator.free(data);
+pub fn gen(
+    comptime T: type,
+    options: GenOptions,
+) Wave(T) {
+    const samples: []const T = generate_samples(T, options);
+    defer options.allocator.free(samples);
 
-    const result: Wave = Wave.init(data, options.allocator, .{
+    const result: Wave(T) = Wave(T){
+        .samples = samples,
+        .allocator = options.allocator,
         .sample_rate = options.sample_rate,
         .channels = options.channels,
-        .bits = options.bits,
-    });
+    };
 
     return result;
 }
 
-fn generate_data(frequency: f32, amplitude: f32, length: usize, sample_rate: f32, allocator: std.mem.Allocator) []const f32 {
-    const radins_per_sec: f32 = frequency * 2.0 * std.math.pi;
+fn generate_samples(
+    comptime T: type,
+    options: GenOptions,
+) []const T {
+    const sample_rate: f32 = @floatFromInt(options.sample_rate);
+    const radins_per_sec: f32 = options.frequency * 2.0 * std.math.pi;
 
-    var result: []f32 = allocator.alloc(f32, length) catch |err| {
+    var result: []f32 = options.allocator.alloc(f32, options.length) catch |err| {
         std.debug.print("{any}\n", .{err});
         @panic("PANIC");
     };
     var i: usize = 0;
 
     while (i < result.len) : (i += 1) {
-        result[i] = std.math.sin(@as(f32, @floatFromInt(i)) * radins_per_sec / sample_rate) * amplitude;
+        result[i] = std.math.sin(@as(f32, @floatFromInt(i)) * radins_per_sec / sample_rate) * options.amplitude;
     }
 
     return result;

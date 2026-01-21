833381ad40e4608f9a4f3c84cbc2766e40b89368
haruki7049 <tontonkirikiri@gmail.com>
2026-01-21 13:04:21 +0900

    Create examples/base_acoustic_guitar.zig

---
+++ examples/base_acoustic_guitar.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const lightmix = @import("lightmix");
+const lightmix_synths = @import("lightmix_synths");
+
+pub fn gen() !lightmix.Wave(f64) {
+    const allocator = std.heap.page_allocator;
+    const result: lightmix.Wave(f64) = lightmix_synths.Basic.KarplusStrong.gen(f64, .{
+        .frequency = 110.0,
+        .amplitude = 1.0,
+        .length = 88200,
+
+        .allocator = allocator,
+        .sample_rate = 44100,
+        .channels = 1,
+
+        .feedback = 0.995,
+    });
+
+    return result;
+}

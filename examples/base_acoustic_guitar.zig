83e880554eda70dc059dee0dccb483e3e5b6d174
haruki7049 <tontonkirikiri@gmail.com>
2026-02-15 18:33:01 +0900

    Update lightmix to 0.18.0 (#2)
    * Bump up lightmix to 0.17.1
    
    * Update
    
    * Update .deps.nix
    
    * WIP
    
    * Bump up lightmix to 0.18.0
    
    * Update
    
    * Update README

--- examples/base_acoustic_guitar.zig
+++ examples/base_acoustic_guitar.zig
@@ -2,9 +2,8 @@ const std = @import("std");
 const lightmix = @import("lightmix");
 const lightmix_synths = @import("lightmix_synths");
 
-pub fn gen() !lightmix.Wave(f64) {
-    const allocator = std.heap.page_allocator;
-    const result: lightmix.Wave(f64) = lightmix_synths.Basic.KarplusStrong.gen(f64, .{
+pub fn gen(allocator: std.mem.Allocator) !lightmix.Wave(f64) {
+    const result: lightmix.Wave(f64) = try lightmix_synths.Basic.KarplusStrong.gen(f64, .{
         .frequency = 110.0,
         .amplitude = 1.0,
         .length = 88200,
